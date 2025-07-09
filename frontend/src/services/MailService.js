e00035b548e6376f97273732724d5142ea1992c2
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-07-09 21:25:45 +0530

    Done

--- frontend/src/services/MailService.js
+++ frontend/src/services/MailService.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 const API_URL = 'http://localhost:5000/api/email'; // Update if deployed
 
 export const sendIdToEmp = async (data) => {
+  console.log("Loaded Email:", process.env.MAIL_ADDRESS);
+console.log("Loaded Password:", process.env.MAIL_PSWD ? "Loaded ✅" : "❌ NOT loaded");
+
   return axios.post(`${API_URL}/send-id-to-emp`, data);
 };
 
